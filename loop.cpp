f93a2a4f9063cf8ca246ae0ba2d5277dd8d7d5c4
Prathamshukla14 <105924481+Prathamshukla14@users.noreply.github.com>
2024-02-03 00:44:12 +0530

    Add files via upload

---
+++ loop.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+   int x;
+   cout<<"enter you age ";
+   cin>>x;
+   if(x>=18){
+    cout<<"you are an adult";
+   }
+   else{
+    cout<<"you are not a adult";
+   }
+
+    return 0;
+}
