a7bfddc572f90042435a83737f0ddac8844f74b5
Prathamshukla14 <105924481+Prathamshukla14@users.noreply.github.com>
2024-02-03 17:06:41 +0530

    Add files via upload

---
+++ for_while_loop.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    for (int i= 1; i<=10; i++)
+    {
+        cout<< "numbers "<<i << endl;
+
+    }
+
+    int a=1;
+    while (a<=5)
+    {
+        cout<<a<<endl;
+        a= a+1;
+    } 
+    
+    int b =1;
+    do
+    {
+      cout<< b<<endl;
+      b=b+1;
+
+    } while (b<2);
+    
+    
+    return 0 ;
+
+
+    
+}
+
+
