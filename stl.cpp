1cc88dac2892af78f6f2375e21e0e48aa96c45ca
Prathamshukla14 <105924481+Prathamshukla14@users.noreply.github.com>
2024-02-08 23:29:10 +0530

    Add files via upload

---
+++ stl.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    pair<int,int> p= {1,3};
+    cout<<p.first<<endl;
+
+    pair<int, int>arr[]={{1,2},{3,4}};
+    cout<<arr[0].second;
+}
